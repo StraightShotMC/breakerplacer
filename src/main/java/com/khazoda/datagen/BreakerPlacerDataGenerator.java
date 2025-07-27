f3846ffd410cc67f1176ea1187e72a36e1e2291d
June|Khazoda <khazoda@gmail.com>
2025-07-27 22:10:22 +0100

    datagen for recipes and recipe advancements

--- src/main/java/com/khazoda/datagen/BreakerPlacerDataGenerator.java
+++ src/main/java/com/khazoda/datagen/BreakerPlacerDataGenerator.java
@@ -1,11 +1,19 @@
 package com.khazoda.datagen;
 
+import com.khazoda.breakerplacer.Constants;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import org.jetbrains.annotations.Nullable;
 
 public class BreakerPlacerDataGenerator implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+  @Override
+  public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+    var pack = fabricDataGenerator.createPack();
+    pack.addProvider(BreakerPlacerRecipeProvider::new);
+  }
 
-	}
+  @Override
+  public @Nullable String getEffectiveModId() {
+    return Constants.NAMESPACE;
+  }
 }
